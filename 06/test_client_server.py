ba8dab388c0ca9b7668e71cec5b965d274831515
lida24 <lidalenkova204@gmail.com>
2023-11-02 23:56:27 +0300

    hw-6 done

--- 06/test_client_server.py
+++ 06/test_client_server.py
@@ -1,37 +1,34 @@
 import unittest
-import server
-import client
+from server import Server, URLProcess, create_parser
+from client import Client
 from unittest import mock
+from queue import Queue
 
 
 class TestServer(unittest.TestCase):
-    def setUp(self) -> None:
-        pass
+    @mock.patch('argparse.ArgumentParser.parse_args', return_value=mock.MagicMock(w=5, k=10))
+    def test_create_parser(self, mock_parse_args):
+        parser = create_parser()
+        args = parser.parse_args()
+        self.assertEqual(args.w, 5)
+        self.assertEqual(args.k, 10)
 
-    def tearDown(self) -> None:
-        pass
+    def test_process_url(self):
+        url_process = URLProcess("https://example.com", 5)
+        most_common_words, processed_url = url_process.process()
+        self.assertEqual(processed_url, "https://example.com")
+        self.assertIsInstance(most_common_words, dict)
 
-    # @mock.patch('requests.get')
-    # def test_server_processing_with_mocks(self, mock_get) -> None:
-    #     server_obj = server.Server(workers_count=10, k_words=7)
-    #     mock_response = mock.Mock()
-    #     mock_response.text = 'This is response text'
-    #     mock_get.return_value = mock_response
-    #     server_obj.sock = mock.Mock()
-    #     mock_socket = server_obj.sock.return_value
-    #     mock_socket.recv.return_value = 'http://test.com'
-    #     server_obj.start()
-    #     mock_get.assert_called_with('http://test.com')
-    #     mock_socket.send.assert_called_with('http://test.com')
-    #     mock_socket.recv.assert_called_with(4096)
+    def test_create_server(self):
+        server = Server(5, 10)
+        self.assertEqual(server.workers_count, 5)
+        self.assertEqual(server.k_words, 10)
 
-    @mock.patch('socket.socket')
-    def test_server_processing(self, mock_socket) -> None:
-        server_obj = server.Server(workers_count=10, k_words=7)
-        server_obj.sock = mock.Mock()
-        server_obj.sock.accept = mock.Mock(return_value=(mock_socket, ''))
-        mock_socket.recv.return_value = b'http://test.com'
 
-        server_obj.start()
-        mock_socket.send.assert_called_with(b'http://test.com')
-        mock_socket.recv.assert_called_with(4096)
+class TestClient(unittest.TestCase):
+    def test_get_urls(self):
+        client = Client("urls.txt", 5)
+        urls = client.get_urls("urls.txt")
+
+        self.assertIsInstance(urls, Queue)
+        self.assertEqual(urls.qsize(), 100)
