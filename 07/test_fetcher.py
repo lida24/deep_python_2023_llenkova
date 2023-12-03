42b03cbd634af19c094e79642d8c31b0d2d4ff40
lida24 <lidalenkova204@gmail.com>
2023-12-03 19:33:26 +0300

    hw-7 fixed

--- 07/test_fetcher.py
+++ 07/test_fetcher.py
@@ -3,20 +3,17 @@ from unittest import mock
 import asyncio
 import aiohttp
 from aiohttp.test_utils import make_mocked_coro
-from fetcher import fetch_url, worker
+from fetcher import fetch_url, worker, batch_fetch
 
 
 class TestFetcher(unittest.IsolatedAsyncioTestCase):
     def setUp(self) -> None:
         self.mock_queue = asyncio.Queue()
-        self.mock_queue.get = make_mocked_coro(return_value="http://test.com")
         self.mock_session = mock.MagicMock()
-        self.mock_tasks = []
 
     def tearDown(self) -> None:
         self.mock_queue = None
         self.mock_session = None
-        self.mock_tasks = None
 
     @mock.patch("aiohttp.ClientSession.get")
     async def test_fetch_url(self, mock_get) -> None:
@@ -25,11 +22,17 @@ class TestFetcher(unittest.IsolatedAsyncioTestCase):
         mock_get.return_value.__aenter__.return_value = mock_response
 
         async with aiohttp.ClientSession() as session:
-            result = await fetch_url(session, "http://test.com")
+            result = await fetch_url(session, "http://example.com")
             self.assertEqual(result, "Test sentence")
 
     @mock.patch("fetcher.fetch_url")
-    async def test_worker_no_url(self, mock_fetch) -> None:
+    async def test_worker_without_url(self, mock_fetch) -> None:
         self.mock_queue.get = make_mocked_coro(return_value=None)
-        await worker(self.mock_session, self.mock_queue, self.mock_tasks)
+        await worker(self.mock_session, self.mock_queue)
         mock_fetch.assert_not_called()
+
+    @mock.patch("fetcher.fetch_url")
+    async def test_worker_with_url(self, mock_fetch) -> None:
+        self.mock_queue.get = make_mocked_coro(return_value="http://example.com")
+        await worker(self.mock_session, self.mock_queue)
+        mock_fetch.assert_called_once_with(self.mock_session, "http://example.com")
